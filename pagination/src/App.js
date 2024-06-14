21ebb1643564dfbd7cf75cbfe2fbad7d3df0536b
kirangowda95-tech <76085458+KiranGowda95-tech@users.noreply.github.com>
2024-06-14 17:26:24 +0000

    pages updated

---
+++ pagination/src/App.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  console.log("entering to pagination");
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const fetchProducts = async () => {
+    console.log("entering into fetchProducts");
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await res.json();
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  };
+  console.log(products);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  const selectPageHandler = (selectedPage) => {
+    setPage(selectedPage);
+  };
+
+  return (
+    <div>
+      {products.length > 0 && (
+        <div className="products">
+          {products.slice(page * 10 - 10, page * 10).map((prod) => {
+            return (
+              <span className="products__single" key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
+      {products.length > 0 && (
+        <div className="pagination">
+          <span>◀</span>
+          {[...Array(products.length / 10)].map((_, i) => {
+            return (
+              <span onClick={() => selectPageHandler(i + 1)} key={i}>
+                {i + 1}
+              </span>
+            );
+          })}
+          <span>▶</span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
